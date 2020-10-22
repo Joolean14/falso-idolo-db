c3843618dd252acaf68ac61d9080372fbdfdea10
Julian Loaiza <you@example.com>
2020-10-22 12:10:46 -0500

    Agregue queries.

--- fi-schema.sql
+++ fi-schema.sql
@@ -61,3 +61,55 @@ CREATE TABLE payment_method (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 
+
+------------------------------------------------------------------------------------------------
+
+--Queries
+
+--7. Busqueda por nombre, precio, categoria, descuentos.
+
+SELECT nombre_producto 
+FROM producto;
+
+SELECT precio_producto 
+FROM producto;
+
+SELECT nombre_producto 
+FROM descripcion;
+
+SELECT descuento
+FROM producto;
+
+--11. Mostrar el inventario en fecha seleccionada.
+
+SELECT stock
+FROM producto;
+
+--12. Mostrar el iva del total de ventas.
+
+SELECT SUM(valor * 0.19)
+FROM transaccion;
+
+--13. Mostrar ventas respecto al punto de equilibrio de la empresa.
+
+SET @punto_equilibrio = 1000;
+
+SELECT SUM(valor - @punto_equilibrio )
+FROM transaccion;
+
+--20. Enviar correo despues de 3 meses de su ultima compra.
+
+--Transaccion --> Usuario
+-- fecha_transaccion --> id_usuario
+
+
+--5. Mostrar perfil de usuario.
+
+SELECT * 
+FROM usuario;
+
+--8. Dias y horarios con mas ventas.
+
+
+--19. Mostrar posibles compras con fidelizacion.
+
